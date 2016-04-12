acf102894cd7e0076f05df6626e624aaa76e2b5a
Jan Godziatkowski <ph.godzio@gmail.com>
2016-04-12 11:22:23 +0200

    add run instance function

--- lab/ec2_get_instance.js
+++ lab/ec2_get_instance.js
@@ -3,7 +3,8 @@
 var AWS = require('aws-sdk');
 
 module.exports = {
-	info: info
+	info: info,
+	runInstance: runInstance
 };
 
 
@@ -22,6 +23,37 @@ function info(request, callback) {
 		}
 		else {
 			callback(null, data);
+		}
+	});
+}
+
+function runInstance( request, callback){
+	var params = {
+		ImageId: 'ami-80f715e0',
+		MaxCount: 1,
+		MinCount: 1,
+		Monitoring: {
+			Enabled: false
+		},
+		InstanceTyoe: 't1.micro'
+	};
+	ec2.runInstance(params, function(err,data){
+		if(err){
+			callback(err);
+		}else{
+			var params = {
+				DryRun: false,
+				InstanceIds: [data.Instances[0].InstanceId]
+			};
+			
+			ec2.describeInstances(params, function(err, data) {
+				if (err) {
+					callback(err); 
+				}
+				else {
+					callback(null, "DNS: " + data.Reservations[0].Instances[0].PublicDnsName + "<br />" + "IP: " + data.Reservations[0].Instances[0].PublicIpAddress);
+				}
+			});
 		}
 	});
 }
